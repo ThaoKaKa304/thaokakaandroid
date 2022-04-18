01838a2519221fc0235c59965a57d7d4e0eaca66
thaokaka <thaotrung1966@gmail.com>
2022-04-18 09:50:26 +0700

    first commit

---
+++ app/src/main/java/com/example/topcarregister/BillDetail.java
@@ -0,0 +1,13 @@
+package com.example.topcarregister;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BillDetail extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bill_detail);
+    }
+}
